5462fc466a5fe41f1a5495044906082da91b9f26
souro18 <sourajit.paul61@gmail.com>
2020-06-22 22:30:10 +0530

    add debounce

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -7,10 +7,19 @@ class AllBooks extends React.Component {
 	state = {
     searchedBooks : []
   }
-  // searchedBooks= []
+
+  debounce = (fn, time) => {
+    let timer;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(()=>fn.apply(context,args), time);
+    }
+  }
+
   updateQuery=(query)=>{
     query =query.trim()
-    console.log("query" +query)
     if(query) {
       BooksAPI.search(query).then((res)=> {
         console.log(res)
@@ -20,51 +29,49 @@ class AllBooks extends React.Component {
         else {
           this.setState({searchedBooks:res})
         }
-    })
+      })
     }
     else {
       this.setState({searchedBooks:[]})
     }
-
   }
 
-  render() {
 
+  render() {
+    const debouncedUpdate = this.debounce(this.updateQuery, 500);
 		return (
 			<div className="search-books">
-            <div className="search-books-bar">
-            <Link to={{
-                  pathname: '/',
-                }} className="close-search">
-                  Close
-                </Link>
-              <div className="search-books-input-wrapper">
-               
-                <input type="text" placeholder="Search by title or author"
-                onChange={(event)=>this.updateQuery(event.target.value)}/>
-
-              </div>
-            </div>
-
-            <div className="search-books-results">
-              {this.state.searchedBooks.length !== 0 ? (
-                <ol className="books-grid">
-                  {this.state.searchedBooks.map((book)=>{
-                    const found = this.props.books.find((b)=>{
-                      return (b.id=== book.id)
-                    });
-                    (found) ? (book.shelf = found.shelf) : book.shelf="none";
-                    return <li key={book.id}><Book book={book}
-                      updateBook={this.props.updateBook}/></li>
-               }
-               )}
-              </ol>
-
-              ):(
-              <p>No Matched Results</p>
-              )}
+        <div className="search-books-bar">
+          <Link to={{
+                pathname: '/',
+              }} className="close-search">
+                Close
+              </Link>
+            <div className="search-books-input-wrapper"> 
+              <input type="text" placeholder="Search by title or author"
+              onChange={(event)=>debouncedUpdate(event.target.value)}/>
             </div>
           </div>
+
+          <div className="search-books-results">
+            {this.state.searchedBooks.length !== 0 ? (
+              <ol className="books-grid">
+                {this.state.searchedBooks.map((book)=>{
+                  const found = this.props.books.find((b)=>{
+                    return (b.id=== book.id)
+                  });
+                  (found) ? (book.shelf = found.shelf) : book.shelf="none";
+                  return <li key={book.id}><Book book={book}
+                    updateBook={this.props.updateBook}/></li>
+              }
+              )}
+            </ol>
+
+            ):(
+            <p>No Matched Results</p>
+            )}
+          </div>
+      </div>
 		);
 	}
 }
