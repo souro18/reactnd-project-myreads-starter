8f590e257206239098b47bd5b65093386eb5bd00
souro18 <sourajit.paul61@gmail.com>
2020-06-22 18:52:03 +0530

    update react version and add lazy loaded routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from 'react-router-dom'
-import AllBooks from './AllBooks'
-import MyReads from './myReads'
+import { Route, Switch } from 'react-router-dom'
+const AllBooks = lazy(()=> import('./AllBooks')); 
+const MyReads = lazy(() => import('./myReads'));
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [],
     currentlyReading : [],
     wantToRead :[],
@@ -59,18 +53,24 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-          <Route path="/search" render={()=>(
+        <Switch>
+          <Route path="/search">
+            <Suspense fallback={<div>Loading...</div>}>
             <AllBooks 
-            books={this.state.books}
-            updateBook= {this.updateBook}/>
-          )}/>
-          <Route exact path="/" render={()=>(
-            <MyReads 
+              books={this.state.books}
+              updateBook= {this.updateBook}/>
+            </Suspense>
+          </Route>
+          <Route exact path="/">
+          <Suspense fallback={<div>Loading...</div>}>
+          <MyReads 
             currentlyReading={this.state.currentlyReading}
             wantToRead={this.state.wantToRead}
             read={this.state.read}
             updateBook= {this.updateBook}/>
-          )}/>
+            </Suspense>
+          </Route>
+        </Switch>
       </div>
     )
   }
